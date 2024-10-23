e85cc3ebb8b01206bda3bf3c53c37828a22b5a26
GZC <gzc18101301997@gmail.com>
2024-10-23 18:12:20 +0200

    second Try

--- MachineTranslationModule.py
+++ MachineTranslationModule.py
@@ -1,8 +1,4 @@
 import torch
 
-if torch.cuda.is_available():
-    device = torch.cuda.current_device()
-    print('Current device:', torch.cuda.get_device_name(device))
-else:
-    print('Failed to find GPU. Will use CPU.')
-    device = 'cpu'
+# Cloing fairseq repo from github
+!git clone https://github.com/facebookresearch/fairseq.git
